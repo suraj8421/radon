8398cdebf03150619e8d7fb9aecd8be8c8bd3bbb
Avijit07 <happyavijit1998@gmail.com>
2022-07-06 19:06:10 +0530

    updated auth

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,17 +1,22 @@
 const jwt = require("jsonwebtoken");
 //////////////////////////////////////authentication///////////////////////////////////////
-const authentication = function(req, res, next) {
+const authentication = function (req, res, next) {
     try {
         const token = req.headers[`x-api-key`]
         if (!token) return res.status(400).send({ status: false, msg: "Token must be present in Headers" });
-        let decodedToken = jwt.verify(token, "ASDFGH3456OKJNBDCFGHJ")
-        if ((!decodedToken)) return res.status(400).send({ status: false, msg: "please enter valid token" })
+        let decodedToken = jwt.verify(token,
+            "ASDFGH3456OKJNBDCFGHJ",
+            function (err, decodedToken) {
+                if (err) {
+                    return res.status(401).send({ status: false, message: "Token is NOT Valid" })
+                }
 
-        req.userId = decodedToken.userId
+                req.userId = decodedToken.userId
+                next();
+            })
+
     } catch (err) {
         return res.status(500).send({ status: false, msg: err.message })
     }
-
-    next()
 }
 module.exports = { authentication }
