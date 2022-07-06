a864727d4e421ba8ab6bc61f42b119c9cbc515c8
BAravind85 <102968059+BAravind85@users.noreply.github.com>
2022-07-06 19:06:42 +0530

    h

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const { now } = require("mongoose");
 //////////////////////////////////////authentication///////////////////////////////////////
 const authentication = function(req, res, next) {
     try {
@@ -6,8 +7,8 @@ const authentication = function(req, res, next) {
         if (!token) return res.status(400).send({ status: false, msg: "Token must be present in Headers" });
         let decodedToken = jwt.verify(token, "ASDFGH3456OKJNBDCFGHJ")
         if ((!decodedToken)) return res.status(400).send({ status: false, msg: "please enter valid token" })
-
-        req.userId = decodedToken.userId
+            //if (Date.now() > decodedToken * 1000) return res.status(401).send({ status: false, message: "login expired" })
+        req.decodedToken = decodedToken
     } catch (err) {
         return res.status(500).send({ status: false, msg: err.message })
     }
