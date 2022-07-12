21d2f666870304fed4525c35f21133f3c5357241
Avijit07 <happyavijit1998@gmail.com>
2022-07-13 01:14:48 +0530

    final code

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -117,7 +117,7 @@ const loginUser = async function (req, res) {
         let token = jwt.sign({
             userId: user._id.toString(),
             iat: Math.floor(Date.now() / 1000),
-            exp: Math.floor(Date.now() / 1000) + 10
+            exp: Math.floor(Date.now() / 1000) + 7600
         },
             "ASDFGH3456OKJNBDCFGHJ"
 
