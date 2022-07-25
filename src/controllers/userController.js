9826eb08a8e8ea31fa4e6a44ed49d3c683abb1e1
BAravind85 <102968059+BAravind85@users.noreply.github.com>
2022-07-25 14:18:19 +0530

    add

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,45 +1,20 @@
 const userModel = require("../models/userModel")
-const aws = require("aws-sdk")
 const bcrypt = require('bcrypt');
-
-
-aws.config.update({
-    accessKeyId: "AKIAY3L35MCRVFM24Q7U",
-    secretAccessKey: "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",
-    region: "ap-south-1"
-})
-
-let uploadFile = async (file) => {
-    return new Promise(function (resolve, reject) {
-        // this function will upload file to aws and return the link
-        let s3 = new aws.S3({ apiVersion: '2006-03-01' }); // we will be using the s3 service of aws
-
-        var uploadParams = {
-            ACL: "public-read",
-            Bucket: "classroom-training-bucket",  //HERE
-            Key: "group69-project5/" + file.originalname, //HERE 
-            Body: file.buffer
-        }
-
-
-        s3.upload(uploadParams, function (err, data) {
-            if (err) {
-                return reject({ "error": err })
-            }
-            // console.log(data)
-            // console.log("file uploaded succesfully")
-            return resolve(data.Location)
-        })
-    })
-}
-
-
-
-
+const {uploadFile}= require('../aws/fileUpload')
+const { isValid,isValidBody, nameRegex, emailRegex,validMobile,passwordRegex,pinRegex}=require('../middleware/valid')
 
 const createUser = async function (req, res) {
     try {
 
+        let body= req.body
+
+        let {fname, lname, email, phone, password, address } = body
+
+        
+        //encrypted passwordHash
+        const hashPassword = bcrypt.hashSync(password, 10);
+        body["password"] = hashPassword
+        //file upload   
         let files = req.files
         if (files && files.length > 0) {
 
@@ -48,21 +23,11 @@ const createUser = async function (req, res) {
         else {
             res.status(400).send({ msg: "No file found" })
         }
-
-        let body= req.body
-
-        let {fname, lname, email, phone, password, address } = body
-
-        
-        const encryptedPassword = async function(enteredPassword){
-            const passwordHash = await bcrypt.hash(enteredPassword, 15)
-            body.password = passwordHash
-        }
-        encryptedPassword(password)
-
         body["profileImage"] = uploadedFileURL
+
+
         let usersData= await userModel.create(body)
-        return res.send(usersData)
+        return res.send({status:true,message:"User created successfully",usersData})
 
 
 
---
+++ src/middleware/valid.js
@@ -0,0 +1,32 @@
+
+
+
+
+const isValid = function(x) {
+    if (typeof x === "undefined" || x === null) return false;
+    if (typeof x === "string" && x.trim().length === 0) return false;
+
+    return true;
+};
+const isValidBody = function(x) {
+    return Object.keys(x).length > 0;
+};
+
+const nameRegex = /^[a-zA-Z\s]+$/
+
+const emailRegex = /^[a-z]{1}[a-z0-9._]{1,100}[@]{1}[a-z]{2,15}[.]{1}[a-z]{2,10}$/
+
+const validMobile = /^[1-9]\d{9}$/;
+
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,15}$/;
+
+const pinRegex = /^([0-9]){6}$/
+module.exports ={
+    isValid,
+    isValidBody,
+    nameRegex,
+    emailRegex,
+    validMobile,
+    passwordRegex,
+    pinRegex
+}
